ea82d8a366353c610cf29979d50f9a9e0dd2b285
o(￣へ￣)o <514668826@qq.com>
2018-05-22 18:17:39 +0800

    system test: check pay type selector

--- test/system/orders_test.rb
+++ test/system/orders_test.rb
@@ -7,7 +7,7 @@ class OrdersTest < ApplicationSystemTestCase
     @order_without_pay_type = orders(:order_without_pay_type)
   end
 
-  test 'check routing number' do
+  test 'check pay type selector' do
     visit store_index_url
 
     first('.catalog li').click_on 'Add to Cart'
@@ -17,9 +17,25 @@ class OrdersTest < ApplicationSystemTestCase
     fill_in 'order_name', with: @order_without_pay_type.name
     fill_in 'order_address', with: @order_without_pay_type.address
     fill_in 'order_email', with: @order_without_pay_type.email
+
     assert_no_selector '#order_routing_number'
+    assert_no_selector '#order_credit_card_number'
+    assert_no_selector '#order_po_number'
 
     select 'Check', from: 'pay_type'
     assert_selector '#order_routing_number'
+    assert_no_selector '#order_credit_card_number'
+    assert_no_selector '#order_po_number'
+
+    select 'Credit card', from: 'pay_type'
+    assert_no_selector '#order_routing_number'
+    assert_selector '#order_credit_card_number'
+    assert_no_selector '#order_po_number'
+
+    select 'Purchase order', from: 'pay_type'
+    assert_no_selector '#order_routing_number'
+    assert_no_selector '#order_credit_card_number'
+    assert_selector '#order_po_number'
+
   end
 end
