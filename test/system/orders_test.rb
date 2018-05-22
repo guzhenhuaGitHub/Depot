e79999ec35baf4a86a8f18062aadedfb4360ddb6
o(￣へ￣)o <514668826@qq.com>
2018-05-22 18:04:19 +0800

    system test: check routing number

--- test/system/orders_test.rb
+++ test/system/orders_test.rb
@@ -1,49 +1,25 @@
-require "application_system_test_case"
+# frozen_string_literal: true
+
+require 'application_system_test_case'
 
 class OrdersTest < ApplicationSystemTestCase
   setup do
-    @order = orders(:one)
+    @order_without_pay_type = orders(:order_without_pay_type)
   end
 
-  test "visiting the index" do
-    visit orders_url
-    assert_selector "h1", text: "Orders"
-  end
+  test 'check routing number' do
+    visit store_index_url
 
-  test "creating a Order" do
-    visit orders_url
-    click_on "New Order"
+    first('.catalog li').click_on 'Add to Cart'
 
-    fill_in "Address", with: @order.address
-    fill_in "Email", with: @order.email
-    fill_in "Name", with: @order.name
-    fill_in "Pay Type", with: @order.pay_type
-    click_on "Create Order"
+    click_on 'Checkout'
 
-    assert_text "Order was successfully created"
-    click_on "Back"
-  end
+    fill_in 'order_name', with: @order_without_pay_type.name
+    fill_in 'order_address', with: @order_without_pay_type.address
+    fill_in 'order_email', with: @order_without_pay_type.email
+    assert_no_selector '#order_routing_number'
 
-  test "updating a Order" do
-    visit orders_url
-    click_on "Edit", match: :first
-
-    fill_in "Address", with: @order.address
-    fill_in "Email", with: @order.email
-    fill_in "Name", with: @order.name
-    fill_in "Pay Type", with: @order.pay_type
-    click_on "Update Order"
-
-    assert_text "Order was successfully updated"
-    click_on "Back"
-  end
-
-  test "destroying a Order" do
-    visit orders_url
-    page.accept_confirm do
-      click_on "Destroy", match: :first
-    end
-
-    assert_text "Order was successfully destroyed"
+    select 'Check', from: 'pay_type'
+    assert_selector '#order_routing_number'
   end
 end
