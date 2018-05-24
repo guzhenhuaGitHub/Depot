ffaab81cf8d7fce46b43983085d0ec2116551ea9
o(￣へ￣)o <514668826@qq.com>
2018-05-24 10:43:39 +0800

    generate mailer

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,20 @@
+# frozen_string_literal: true
+
 # Load the Rails application.
 require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Depot::Application.configure do
+#   config.action_mailer.delivery_method = :stmp
+#
+#   config.action_mailer.stmp_settings = {
+#     address:                'stmp.qq.com',
+#     port:                   '465', # '587'
+#     authentication:         'plain',
+#     user_name:              '514668826',
+#     password:               'gzh19901105',
+#     enable_starttls_auto:   true
+#   }
+# end
