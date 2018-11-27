d5cc627f617815cfbd7f1334087bf01c547d5e45
o(￣へ￣)o <514668826@qq.com>
2018-11-27 18:52:34 +0800

    rubocop configure and fix

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Product < ApplicationRecord
-  has_many :line_items
+  has_many :line_items, dependent: :destroy
   has_many :orders, through: :line_items
 
   before_destroy :ensure_not_referenced_by_any_line_item
